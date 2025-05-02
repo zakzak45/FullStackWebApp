42e6f7841aa110c1369a7802f4db181bb6092054
zakzak45 <147825634+zakzak45@users.noreply.github.com.>
2025-05-02 23:06:35 +0200

    initial commit

---
+++ FullStackWebApp/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import HomePage from './HomePage'
+
+function App() {
+
+
+  return (
+    <>
+      <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ FullStackWebApp/src/HomePage.tsx
@@ -0,0 +1,19 @@
+const HomePage = () => {
+    return (
+        <>
+            <div className="home-page">
+                <h2>FrontEndWebApp</h2>
+
+            </div>
+            <div className="main">
+
+                <label for="student" id="St">If you are student click here</label>
+                <button id="student"><a href="#">Student</a></button>
+                <label for="tutor" id="Tt">If youre a tutor click here</label>
+                <button id="tutor"><a href="#">Tutor</a></button>
+            </div>
+        </>
+    );
+}
+
+export default HomePage;
